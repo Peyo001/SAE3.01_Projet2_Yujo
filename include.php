7a3c5fb4054be767615ef47d61a9445ab7411bac
Thibaut ROSA <rosa.thibaut@outlook.fr>
2025-11-07 14:47:43 +0100

    Ajout de la classe Reponse

--- include.php
+++ include.php
@@ -2,4 +2,5 @@
 //information de connexion a la base de donnee
 require_once __DIR__ . '/config/configDatabase.php';
 require_once __DIR__ . '/src/Database/DataBase.php';
+require_once __DIR__ . '/src/Model/response.class.php';
 ?>
