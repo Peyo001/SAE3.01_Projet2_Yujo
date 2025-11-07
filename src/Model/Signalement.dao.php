ecb410b9b0256069adc1eec900053e50b2fa5b7f
Michelena Peyo <peyo.michelena@gmail.com>
2025-11-07 12:30:08 +0100

    Creation Signalement modele DAO

---
+++ src/Model/Signalement.dao.php
@@ -0,0 +1,46 @@
+<?php
+
+class SignalementDao
+{
+    private ?PDO $pdo;
+
+    public function __construct(?PDO $pdo=null)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function getPdo(): ?PDO
+    {
+        return $this->pdo;
+    }
+
+    public function setPdo(PDO $pdo): void
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function getAllSignalements(): array
+    {
+        $signalements = [];
+        $stmt = $this->pdo->query("SELECT id, raison FROM signalements");
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $signalement = new Signalement($row['id'], $row['raison']);
+            $signalements[] = $signalement;
+        }
+        return $signalements;
+    }
+
+    public function getSignalementById(int $id): ?Signalement
+    {
+        $stmt = $this->pdo->prepare("SELECT id, raison FROM signalements WHERE id = :id");
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($row) {
+            return new Signalement($row['id'], $row['raison']);
+        }
+        return null;
+    }
+
+}
+?>
