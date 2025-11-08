2a6f215eed44c69b455cc4227055c8e1d60d3982
Mateo CHAMBON--RANGO <mxcr87@gmail.com>
2025-11-08 16:22:02 +0100

    Création des fichiers Database.php et Room.class.php

---
+++ src/Model/Room.class.php
@@ -0,0 +1,72 @@
+<?php
+    // Création de la classe Room
+    
+    class Room {
+        private ?int $idRoom;
+        private ?string $nom;
+        private ?string $visibilite;
+        private ?datetime $dateCreation;
+        private ?int $nbVisit;
+        private ?int $idCreateur;
+
+        public function __construct(?int $idRoom, ?string $nom, ?string $visibilite, ?datetime $dateCreation, ?int $nbVisit, ?int $idCreateur) {
+            $this->setIdRoom($idRoom);
+            $this->setNom($nom);
+            $this->setVisibilite($visibilite);
+            $this->setDateCreation($dateCreation);
+            $this->setNbVisit($nbVisit);
+            $this->setIdCreateur($idCreateur);
+        }
+
+
+        // Getters
+        public function getIdRoom(): ?int {
+            return $this->idRoom;
+        }
+
+        public function getNom(): ?string {
+            return $this->nom;
+        }
+
+        public function getVisibilite(): ?string {
+            return $this->visibilite;
+        }
+
+        public function getDateCreation(): ?datetime {
+            return $this->dateCreation;
+        }
+
+        public function getNbVisit(): ?int {
+            return $this->nbVisit;
+        }
+
+        public function getIdCreateur(): ?int {
+            return $this->idCreateur;
+        }
+
+
+        // Setters
+        public function setIdRoom(?int $idRoom): void {
+            $this->idRoom = $idRoom;
+        }
+
+        public function setNom(?string $nom): void {
+            $this->nom = $nom;
+        }
+
+        public function setVisibilite(?string $visibilite): void {
+            $this->visibilite = $visibilite;
+        }
+
+        public function setDateCreation(?datetime $dateCreation): void {
+            $this->dateCreation = $dateCreation;
+        }
+
+        public function setnbVisit(?int $nbVisit): void {
+            $this->nbVisit = $nbVisit;
+        }
+
+        public function setIdCreateur(?int $idCreateur): void {
+            $this->idCreateur = $idCreateur;
+        }
+    }
