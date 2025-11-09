9e4cb83155caa413aa4586e9ac60402f5ba2992a
Mateo CHAMBON--RANGO <mxcr87@gmail.com>
2025-11-09 17:28:24 +0100

    Création de Room.dao.php qui complète le modèle de la room

---
+++ src/Model/Room.dao.php
@@ -0,0 +1,54 @@
+<?php
+    class RoomDao {
+
+        // ATTRIBUT
+        private PDO $conn;
+
+        // CONSTRUCTEUR
+        public function __construct() {
+            $this->conn = Database::getInstance()->getConnection();
+        }
+
+        //DESTRUCTEUR
+        public function __destruct() {
+            Database::getInstance()->__destruct();
+        }
+
+        // METHODES
+        public function findRoomById(int $id): ?Room {
+            $stmt = $this->conn->prepare("SELECT idRoom, nom, visibilite, dateCreation, nbVisit, idCreateur FROM ROOM WHERE idRoom = :id");
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($row) {
+                    return new Room(
+                    $row['idRoom'],
+                    $row['nom'],
+                    $row['visibilite'],
+                    $row['dateCreation'],
+                    $row['nbVisit'],
+                    $row['idCreateur']);
+            }
+            return null;
+        }
+
+        public function createRoom(Room $room): bool {
+            $stmt = $this->conn->prepare("INSERT INTO ROOM (nom, visibilite, dateCreation, nbVisit, idCreateur) VALUES (:nom, :visibilite, :dateCreation, :nbVisit, :idCreateur)");
+            $stmt->bindValue(':nom', $user->getNom(), PDO::PARAM_STR);
+            $stmt->bindValue(':visibilite', $user->getVisibilite(), PDO::PARAM_STR);
+            $stmt->bindValue(':dateCreation', $user->getDateCreation(), PDO::PARAM_STR);
+            $stmt->bindValue(':nbVisit', $user->getNbVisit(), PDO::PARAM_INT);
+            $stmt->bindValue(':idCreateur', $user->getIdCreateur(), PDO::PARAM_INT);
+
+            return $stmt->execute();
+        }
+    
+        public function deleteRoom(int $id): bool {
+            $stmt = $this->conn->prepare("DELETE FROM ROOM WHERE idRoom = :id");
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+            return $stmt->execute();
+        }
+    }
+?>
