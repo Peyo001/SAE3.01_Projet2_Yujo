0b88110f0efdfc7b194181182728d006ec20c0da
Mateo CHAMBON--RANGO <mxcr87@gmail.com>
2025-11-09 17:26:53 +0100

    Création de DataBase.php pour faire la connection PDO

---
+++ src/Database/DataBase.php
@@ -0,0 +1,50 @@
+<?php
+
+class Database
+{
+    private static ?Database $instance = null; // Singleton
+    private PDO $conn;                          // Connexion PDO
+
+    
+    // Constructeur privé
+    private function __construct()
+    {
+        try {
+            // Connexion PDO simple
+            $this->conn = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
+
+            // Définir le charset UTF-8
+            $this->conn->exec("SET NAMES utf8mb4");
+        } catch (Exception $e) {
+            throw new Exception("Erreur PDO : " . $e->getMessage());
+        }
+        echo "Connexion à la base de données réussie.";
+    }
+
+
+    //destructeur
+    public function __destruct()
+    {
+        
+    }   
+
+    // Récupérer l'instance unique
+    public static function getInstance(): Database
+    {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    // Récupérer la connexion PDO
+    public function getConnection(): PDO
+    {
+        return $this->conn;
+    }
+
+    // Empêcher clonage et désérialisation
+    private function __clone() {}
+    private function __wakeup() {}
+}
+?>
