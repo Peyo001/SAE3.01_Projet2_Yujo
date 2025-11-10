4ac957b65ebeea744d036703aec6c3b522be000d
Michelena Peyo <peyo.michelena@gmail.com>
2025-11-10 08:31:45 +0100

    Ajout de Achat dans le include.php et ajout balise de fin class Signalement

--- include.php
+++ include.php
@@ -2,4 +2,10 @@
 //information de connexion a la base de donnee
 require_once __DIR__ . '/config/configDatabase.php';
 require_once __DIR__ . '/src/Database/DataBase.php';
+
+// Autoloading des classes
+require_once __DIR__ . '/src/Model/Signalement.class.php';
+require_once __DIR__ . '/src/Model/Signalement.dao.php';
+require_once __DIR__ . '/src/Model/Achat.class.php';
+require_once __DIR__ . '/src/Model/Achat.dao.php';
 ?>
