5b810d84b6662728bf64bcca015408c755a64068
Mateo CHAMBON--RANGO <mxcr87@gmail.com>
2025-11-10 08:26:45 +0100

    Modification de Room.dao.php en ajoutant fonction pour tout afficher

--- config/configDatabase.php
+++ config/configDatabase.php
@@ -1,21 +1,6 @@
 <?php
 
-    class Database {
-        private string $host = "localhost";
-        private string $dbName = "SAE_301";
-        private string $username = "mxcr";
-        private string $password = "spiderman";
-        private ?PDO $conn = null;
-
-        public function connect(): PDO {
-            if ($this->conn === null) {
-                try {
-                    $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbName};charset=utf8", $this->username, $this->password);
-                    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                } catch (PDOException $e) {
-                    die("Erreur de connexion à la base de données : " . $e->getMessage());
-                }
-            }
-            return $this->conn;
-        }
-    }
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'SAE_301');
+    define('DB_USER', 'mxcr');
+    define('DB_PASSWORD', 'spiderman');
