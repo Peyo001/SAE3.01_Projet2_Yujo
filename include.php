1192932142ac7179f4b7892382b7635bac85a308
Lucas VIVIEN <lvivien001@iutbayonne.univ-pau.fr>
2025-11-10 08:33:23 +0100

    Modification du nom Post

--- include.php
+++ include.php
@@ -4,6 +4,6 @@ require_once __DIR__ . '/config/configDatabase.php';
 require_once __DIR__ . '/src/Database/DataBase.php';
 
 //chargement des modèles
-require_once __DIR__ . '/src/Model/post.class.php';
-require_once __DIR__ . '/src/Model/post.dao.php';
+require_once __DIR__ . '/src/Model/Post.class.php';
+require_once __DIR__ . '/src/Model/Post.dao.php';
 ?>
