7aa44b4f2e7b811ee082097946e09ca26e695313
Ethan Bernier <Ethan.bernier66@gmail.com>
2025-11-14 16:10:59 +0100

    Ajout de commentaires

--- src/Model/Message.class.php
+++ src/Model/Message.class.php
@@ -1,18 +1,28 @@
 <?php
 class Message {
+    // ATTRIBUTS
     private int $idMessage;
     private string $contenuMessage;
     private ?string $dateEnvoi;
     private int $idUtilisateur;
     private int $idGroupe;
 
+    //CONSTRUCTEUR
     public function __construct(int $idMessage, string $contenuMessage, ?string $dateEnvoi, int $idUtilisateur) {
         $this->idMessage = $idMessage;
         $this->contenuMessage = $contenuMessage;
         $this->dateEnvoi = $dateEnvoi;
         $this->idUtilisateur = $idUtilisateur;
+        $this->idGroupe = $idGroupe;    
     }
 
+    // DESTRUCTEUR
+    public function __destruct() {
+        // Rien à nettoyer ici
+    }
+
+    //ENCAPSULATION
+    //GETTERS
     public function getIdMessage(): int {
         return $this->idMessage;
     }
@@ -29,11 +39,42 @@ class Message {
         return $this->idUtilisateur;
     }
     
+    public function getIdGroupe(): int
+    {
+        return $this->idGroupe;
+    }
 
-
+    //SETTERS
     public function setIdMessage(int $idMessage): void {
         $this->idMessage = $idMessage;
     }
+
+    public function setContenuMessage(string $contenuMessage): void {
+        $this->contenuMessage = $contenuMessage;
+    }
+
+    public function setDateEnvoi(?string $dateEnvoi): void {
+        $this->dateEnvoi = $dateEnvoi;
+    }
+
+    public function setIdUtilisateur(int $idUtilisateur): void {
+        $this->idUtilisateur = $idUtilisateur;
+    }
+
+    public function setIdGroupe(int $idGroupe): void
+    {
+        $this->idGroupe = $idGroupe;
+    }
+
+    //METHODES
+    public function afficherMessage(): string {
+        return "[" . $this->dateEnvoi . "] User " . $this->idUtilisateur . ": " . $this->contenuMessage;
+    }
+
+    public function __toString(): string {
+        return $this->afficherMessage();
+    }
+    
 
     
    
