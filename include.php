2286c4f914794d8bddae1391f565786a5f7b286d
Lucas Vivien <126165298+LucasVvn@users.noreply.github.com>
2025-11-17 16:02:10 +0100

    modification du include.php pour le configDatabase en json

--- include.php
+++ include.php
@@ -1,6 +1,6 @@
 <?php
 //information de connexion a la base de donnee
-require_once __DIR__ . '/config/configDatabase.php';
+require_once __DIR__ . '/config/configDatabase.json';
 require_once __DIR__ . '/src/Database/DataBase.php';
 require_once __DIR__ . '/config/twig.php';
 
