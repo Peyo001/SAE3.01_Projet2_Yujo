88aebc945c65012e085bad2d28bfc69160d83626
Michelena Peyo <peyo.michelena@gmail.com>
2025-11-25 19:08:06 +0100

    Modification de la classe signalement et signalement dao

--- src/Model/Signalement.class.php
+++ src/Model/Signalement.class.php
@@ -1,24 +1,33 @@
 <?php
 
 class Signalement {
-    private $id;
-    private $raison;
+    // On met ?int pour dire que l'id peut être null (avant insertion)
+    private ?int $id; 
+    private ?string $raison;
 
-    public function __construct($id, $raison) {
+    // Constructeur : l'ID est null par défaut s'il n'est pas fourni
+    public function __construct(?int $id = null, ?string $raison = null) {
         $this->id = $id;
         $this->raison = $raison;
     }
 
+    public function __destruct() { }
+
     // Getters and Setters
-    public function getId() {
+    public function getId(): ?int {
         return $this->id;
     }
 
-    public function getRaison() {
-        return $this->raison    ;
+    public function setId(int $id): void {
+        $this->id = $id;
     }
 
-    public function setRaison($raison) {
+    public function getRaison(): ?string {
+        return $this->raison;
+    }
+
+    public function setRaison(?string $raison): void {
         $this->raison = $raison;
     }
 }
+?>
