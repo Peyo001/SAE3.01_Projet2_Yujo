7182a5863fd49f04ec44aae715e79bc5f6a5692c
Mateo CHAMBON--RANGO <mxcr87@gmail.com>
2025-12-01 18:31:22 +0100

    Creation des controllers de Room et Objet

--- src/Controller/controller_objet.class.php
+++ src/Controller/controller_objet.class.php
@@ -2,11 +2,11 @@
 
     class ControllerObjet extends Controller {
 
-        public function __construct(\Twig\Environment $twig, \Twig\Loader\FilesystemLoader $loader) {
-            parent::__construct($twig, $loader);
+        public function __construct(\Twig\Loader\FilesystemLoader $loader, \Twig\Environment $twig) {
+            parent::__construct($loader, $twig);
         }
 
-        public function lister() {
+        public function lister(): void {
             $idRoom = $_GET['idRoom'] ?? null;
             $managerObjet = new ObjetDao();
 
@@ -17,53 +17,62 @@
                 $objets = $managerObjet->findAll();
             }
 
-            echo $this->getTwig()->render('(Ex:objets_list.twig)', [
+            echo $this->getTwig()->render('liste_objets.twig)', [
                 'objets' => $objets,
-                'idRoom' => $idRoom
+                'title' => 'Liste des objets de la boutique'
             ]);
         }
 
-        public function afficher() {
-            $idObjet = $_GET['idObjet'] ?? null;
-            if (!$idObjet) {
-                die("Erreur : aucun objet spécifié.");
+        public function afficher(): void {
+            if (!isset($_GET['idObjet'])) {
+                header('Location: index.php?controleur=objet&methode=lister');
             }
 
             $managerObjet = new ObjetDao();
-            $objet = $managerObjet->find($idObjet);
+            $objet = $managerObjet->find($_GET['idObjet']);
             if (!$objet) {
                 die("Objet introuvable.");
             }
 
-            echo $this->getTwig()->render('(Ex:objet.twig)', [
+            echo $this->getTwig()->render('objet.twig', [
                 'objet' => $objet
             ]);
         }
 
-        public function creer() {
-            $idRoom = $_GET['idRoom'] ?? null;
-            if (!$idRoom) {
-                die("Erreur : aucune room spécifiée.");
-            }
+        public function afficherFormulaireInsertion(): void
+        {
+            echo $this->getTwig()->render('ajout_objet.twig', [
+                'menu' => 'nouvel_objet'
+            ]);
+        }
 
+        public function traiterFormulaireInsertion() {
             if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-                echo $this->getTwig()->render('(Ex:objet_create.twig)', [
-                    'idRoom' => $idRoom
-                ]);
-                return;
+                header('Location: index.php?controleur=objet&methode=afficherFormulaireInsertion');
+                exit;
             }
 
             // Récupérer données du formulaire
-            $description = $_POST['description'];
+            $description = $_POST['description'] ?? '';
             $modele3dPath = $_POST['modele3dPath'];
             $prix = (int)$_POST['prix'];
 
-            $objet = new Objet(null, $description, $modele3dPath, $prix, $idRoom);
-            $managerObjet = new ObjetDao();
-            $managerObjet->createObjet($objet);
+            $objet = new Objet();
+            $objet->setDescription($description);
+            $objet->setModele3dPath($modele3dPath);
+            $objet->setPrix($prix);
+            $objet->setIdRoom($idRoom);
 
-            header("(Ex:Location: index.php?controller=room&action=afficher&idRoom=)".$idRoom);
-            exit;
+            $managerObjet = new ObjetDao();
+            $succes = $managerObjet->createObjet($objet);
+
+            if ($succes) {
+                header('Location: index.php?controleur=objet&methode=lister');
+                exit;
+            }
+            else {
+                throw new Exception("Erreur lors de la création de l'objet.");
+            }
         }
 
         public function modifier() {
