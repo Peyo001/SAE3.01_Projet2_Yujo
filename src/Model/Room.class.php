38250737cc8f300df21220c2f7763b482686efab
Mateo CHAMBON--RANGO <mxcr87@gmail.com>
2025-12-01 18:32:06 +0100

    Modification de la classe Room

--- src/Model/Room.class.php
+++ src/Model/Room.class.php
@@ -5,20 +5,16 @@
         private ?int $idRoom;
         private ?string $nom;
         private ?string $visibilite;
-        private ?string $dateCreation;
-        private ?int $nbVisit;
-        private ?int $idCreateur;   // incorrect il faut enlever et créer une classe Créer
+        private ?int $nbVisit = 0;
+        private ?array $objets;
 
-        // attribut métier
-        private ?array $objets = [];    // liste d'objets présents dans la room
 
-        public function __construct(?int $idRoom, ?string $nom, ?string $visibilite, ?string $dateCreation, ?int $nbVisit, ?int $idCreateur) {
+        public function __construct(?int $idRoom = null, ?string $nom = null, ?string $visibilite = null, ?int $nbVisit = null, ?array $objets = null) {
             $this->setIdRoom($idRoom);
             $this->setNom($nom);
             $this->setVisibilite($visibilite);
-            $this->setDateCreation($dateCreation);
             $this->setNbVisit($nbVisit);
-            $this->setIdCreateur($idCreateur);
+            $this->setObjets($objets);
         }
 
 
@@ -35,19 +31,17 @@
             return $this->visibilite;
         }
 
-        public function getDateCreation(): ?string {
-            return $this->dateCreation;
-        }
-
         public function getNbVisit(): ?int {
             return $this->nbVisit;
         }
 
-        public function getIdCreateur(): ?int {
-            return $this->idCreateur;
-        }
-
-        public function getObjets(): array {
+        public function getObjets(): ?array {
+            if ($this->objets == null) {
+                $db = Bd::getInstance();
+                $pdo = $db->getConnection();
+                $manager = new ObjetsDao($pdo);
+                $this->objets = $manager->findObjetsByRoom($this->idRoom);
+            }
             return $this->objets;
         }
 
@@ -64,23 +58,11 @@
             $this->visibilite = $visibilite;
         }
 
-        public function setDateCreation(?string $dateCreation): void {
-            $this->dateCreation = $dateCreation;
-        }
-
         public function setNbVisit(?int $nbVisit): void {
             $this->nbVisit = $nbVisit;
         }
 
-        public function setIdCreateur(?int $idCreateur): void {
-            $this->idCreateur = $idCreateur;
-        }
-
-        public function setObjets(array $objets): void {
+        public function setObjets(?array $objets): void {
             $this->objets = $objets;
-        }
-
-        public function addObjet(Objet $objet): void {
-            $this->objets[] = $objet;
         }
     }
