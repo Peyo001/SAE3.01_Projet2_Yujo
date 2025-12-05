e449bb0338c1e3ab20bfce8c707ca914de355ba5
BERNIER Ethan <ethan.bernier66@gmail.com>
2025-12-06 00:13:40 +0100

    Séparation en dossier Dao et class des modèles

--- include.php
+++ include.php
@@ -1,43 +1,29 @@
 <?php
 
-// Autoloading via Composer
+// Charge l'autoload Composer et les configurations de base
 require_once __DIR__ . '/vendor/autoload.php';
-
-//information de connexion a la base de donnee
 require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/src/Database/DataBase.php';
 require_once __DIR__ . '/config/twig.php';
 
-// Autoloading des classes
-require_once __DIR__ . '/src/Model/Signalement.class.php';
-require_once __DIR__ . '/src/Model/Signalement.dao.php';
-require_once __DIR__ . '/src/Model/Achat.class.php';
-require_once __DIR__ . '/src/Model/Achat.dao.php';
-require_once __DIR__ . '/src/Model/Room.class.php';
-require_once __DIR__ . '/src/Model/Room.dao.php';
-require_once __DIR__ . '/src/Model/Utilisateur.class.php';
-require_once __DIR__ . '/src/Model/Utilisateur.dao.php';
-require_once __DIR__ . '/src/Model/Post.class.php';
-require_once __DIR__ . '/src/Model/Post.dao.php';
-require_once __DIR__ . '/src/Model/Reponse.class.php';
-require_once __DIR__ . '/src/Model/Reponse.dao.php';
-require_once __DIR__ . '/src/Model/Sanction.class.php';
-require_once __DIR__ . '/src/Model/Sanction.dao.php';
-require_once __DIR__ . '/src/Model/Ami.class.php';
-require_once __DIR__ . '/src/Model/Ami.dao.php';
-require_once __DIR__ . '/src/Model/Objet.class.php';
-require_once __DIR__ . '/src/Model/Objet.dao.php';
-require_once __DIR__ . '/src/Model/Groupe.class.php';
-require_once __DIR__ . '/src/Model/Groupe.dao.php';
+// Charge automatiquement toutes les classes métiers, DAO et controllers selon l'arborescence du projet
+$autoloadDirs = [
+	__DIR__ . '/src/Model/Class',
+	__DIR__ . '/src/Model/DAO',
+	__DIR__ . '/src/Controller',
+];
 
-//Chargement des controllers
-require_once __DIR__ . '/src/Controller/controller.class.php';
-require_once __DIR__ . '/src/Controller/controller_factory.class.php';
-require_once __DIR__ . '/src/Controller/controller_post.class.php';
-require_once __DIR__ . '/src/Controller/controller_signalement.class.php';
-require_once __DIR__ . '/src/Controller/controller_room.class.php';
-require_once __DIR__ . '/src/Controller/controller_objet.class.php';
-require_once __DIR__ . '/src/Controller/controller_reponse.class.php';
-require_once __DIR__ . '/src/Controller/controller_utilisateur.class.php';
-require_once __DIR__ . '/src/Controller/controller_groupe.class.php';
+
+// Parcours les répertoires spécifiés et inclut tous les fichiers PHP qu'ils contiennent
+foreach ($autoloadDirs as $dir) {
+	if (!is_dir($dir)) {
+		continue;
+	}
+
+    // Inclut tous les fichiers PHP dans le répertoire courant
+	foreach (glob($dir . '/*.php') as $file) {
+		require_once $file;
+	}
+}
+
 ?>
