9282ac542e90f745df8b657ea3c06c15b1c168dd
BERNIER Ethan <ethan.bernier66@gmail.com>
2025-12-07 02:38:39 +0100

    Ajout de bootstrap sur les pages Inscription et connexion

--- public/test_objet.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "Objet.php";
-
-$objet = new Objet(1, "Chaise en bois", "/models/chaise.glb", 20);
-
-echo $objet->getIdObjet();        // Doit afficher 1
-echo $objet->getDescription();    // Chaise en bois
-echo $objet->getModele3dPath();   // /models/chaise.glb
-echo $objet->getPrix();           // 20
