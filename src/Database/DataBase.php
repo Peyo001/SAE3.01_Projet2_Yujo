8966d07b70c9a03fd03e79e40eb3ddd78fc65eff
BERNIER Ethan <ethan.bernier66@gmail.com>
2025-12-08 14:39:19 +0100

    Ajout de commentaires sur la classe DataBase.php

--- src/Database/DataBase.php
+++ src/Database/DataBase.php
@@ -1,10 +1,31 @@
 <?php
-
+/**
+ * Classe de gestion de la connexion à la base de données en utilisant le pattern Singleton.
+ * 
+ * Cette classe assure qu'une seule instance de connexion à la base de données est créée
+ * et fournit un accès global à cette instance.
+ * 
+ * Exemple d'utilisation :
+ * $db = Database::getInstance();
+ * $conn = $db->getConnection();
+ */
 class Database
 {
+
+    //Attributs
+    // Instance unique de la classe Database
     private static ?Database $instance = null;
+    // Connexion PDO à la base de données
     private PDO $conn;                        
 
+    //Constructeur
+    /** Constructeur privé pour empêcher l'instanciation directe.
+     * 
+     * Initialise la connexion à la base de données en utilisant les paramètres
+     * définis dans la configuration.
+     * 
+     * @throws Exception Si la connexion à la base de données échoue.
+    */
     private function __construct()
     {
         try {
@@ -22,6 +43,12 @@ class Database
         }
     }
 
+
+    /** 
+     * Obtient l'instance unique de la classe Database.
+     * 
+     * @return Database L'instance unique de la classe Database.
+     */
     public static function getInstance(): Database
     {
         if (self::$instance === null) {
